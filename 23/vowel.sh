d52d0a30f1a02f8b50c297bf754dd7e4819df54d
Fazil <mfazilkp7@gmail.com>
2023-04-18 17:44:00 +0530

    23 changed

--- 23/vowel.sh
+++ 23/vowel.sh
@@ -1,35 +1,12 @@
 #!/bin/bash
 
-echo "Enter a string: "
+echo "Enter a string:"
 read string
 
-# Convert all characters to lowercase
-string=$(echo $string | tr '[:upper:]' '[:lower:]')
+vowels=$(echo $string | grep -o -i '[aeiou]' | wc -l)
+consonants=$(echo $string | grep -o -i '[^aeiou[:punct:][:space:][:digit:]]' | wc -l)
+numbers=$(echo $string | grep -o -i '[[:digit:]]' | wc -l)
 
-# Initialize variables
-vowels=0
-consonants=0
-numbers=0
-
-# Loop through each character in the string
-for (( i=0; i<${#string}; i++ )); do
-    char=${string:$i:1}
-
-    # Check if the character is a vowel
-    if [[ $char =~ [aeiou] ]]; then
-        ((vowels++))
-
-    # Check if the character is a consonant
-    elif [[ $char =~ [[:alpha:]] ]]; then
-        ((consonants++))
-
-    # Check if the character is a number
-    elif [[ $char =~ [[:digit:]] ]]; then
-        ((numbers++))
-    fi
-done
-
-# Output the results
 echo "Number of vowels: $vowels"
 echo "Number of consonants: $consonants"
 echo "Number of numbers: $numbers"
